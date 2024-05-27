f8829397688fc638f09047c2ac1eef48ae5921f1
MrXetwy21 <94748860+MrXetwy21@users.noreply.github.com>
2024-05-27 13:41:49 -0600

    Add files via upload

---
+++ assets/js/recaptcha.js
@@ -0,0 +1,40 @@
+
+function validateRecaptcha() {
+    const recaptchaResponse = document.getElementById('g-recaptcha-response').value;
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+
+    if (recaptchaResponse === '') {
+        Swal.fire({
+            icon: "error",
+            title: "Error!",
+            text: "Please complete the reCAPTCHA!",
+          });
+        return false;
+    }
+
+    sendEmail(name, email);
+    return false;
+}
+
+function sendEmail(name, email) {
+    emailjs.send('service_o62f7iq', 'template_7gcf31q', {
+        name: name,
+        email: email,
+    })
+    .then(function(response) {
+        
+        console.log('SUCCESS!', response.status, response.text);
+        Swal.fire({
+            icon: "success",
+            title: "Registro exitoso!",
+            text: "Te hemos enviado un correo de confirmación. Revisa tu bandeja de entrada o spam.",
+          });
+    }, function(error) {
+        Swal.fire({
+            icon: "error",
+            title: "Ha ocurrido un error",
+            text: error,
+          });
+    });
+}
